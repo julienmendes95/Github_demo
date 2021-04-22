20a4dca5d70c45f3f1e36d3b3f0696881362e3e7
julienmendes95 <56668185+julienmendes95@users.noreply.github.com>
2021-04-22 18:09:12 +0200

    PEP warnings fixed

--- Human.py
+++ Human.py
@@ -1,48 +1,49 @@
 class Human:
 
-	def __init__(self,name,gender,age,height,weight):
+	def __init__(self, name, gender, age, height, weight):
 		self.name = name
 		self.gender = gender
 		self.age = age
 		self.height = height
 		self.weight = weight
+		self.life = None
 
 	def born(self):
 		self.age = 0
 		self.life = True
 
 	def grow(self):
-		self.age +=1
+		self.age += 1
 		if self.age < 21:
 			self.height += 0.05  
 	
-	def gain(self,diffweight):
+	def gain(self, diffweight):
 		self.weight += diffweight
 
-	def loose(self,diffweight):
+	def loose(self, diffweight):
 		self.loose -= diffweight
 
-	
 	def dead(self):
 		self.life = False
 
 			
 class Man(Human):
-	def __init__(self,name,age,height,weight):
-		super().__init__(name,'Man',age,height,weight)
+	def __init__(self, name, age, height, weight):
+		super().__init__(name, 'Man', age, height, weight)
+
 
 class Woman(Human):
-	def __init__(self,name,age,height,weight):
-		super().__init__(name,'Woman',age,height,weight) 
-		
-def life_cycle(gender,height0,weight0,age_death):
-	name = 'A '+ gender
-	h = Human(name,gender,0,height0,weight0)
+	def __init__(self, name, age, height, weight):
+		super().__init__(name, 'Woman', age, height, weight)
+
+
+def life_cycle(gender, height0, weight0, age_death):
+	name = 'A ' + gender
+	h = Human(name, gender, 0, height0, weight0)
 
 	for n in range(age_death+1):
 		h.grow()
 		
 	h.dead()
 
-
-#life_cycle(gender='Man',height0=0.8,weight0=3,age_death=90)
+#life_cycle(gender='Man', height0=0.8, weight0=3, age_death=90)
